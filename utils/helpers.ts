a6a4a0b029a2eff50381f95a286f00a40d60ff56
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2023-12-20 00:45:27 +0500

    fix failed deployment

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,5 +1,6 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { ProjectType } from "../data/projects";
 
 function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -7,7 +8,7 @@ function cn(...inputs: ClassValue[]) {
 
 const colors = ["#255FBC", "#50B2BE", "#7E478B", "#FFA5A1", "#FFCA28"];
 
-const getDividedData = (data, type) => {
+const getDividedData = (data: ProjectType[] , type: string) => {
   let totalLength = data.length;
   let dividedWouldBe = totalLength / 2;
   let firstHalf = data.slice(0, dividedWouldBe);
