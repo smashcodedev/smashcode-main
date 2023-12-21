10dc65427290a44f51c5dd201e646fa5254cc19e
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2023-12-21 11:43:14 +0500

    Refactor Copywrite component

--- src/components/Footer/Copywrite.tsx
+++ src/components/Footer/Copywrite.tsx
@@ -1,25 +1,19 @@
+import Link from "next/link";
+
 const Copywrite: React.FC = () => {
   return (
-    <div className="copyright-area">
-      <div className="container">
-        <div className="row justify-content-center align-items-center">
-          <div className="col-xl-6 col-lg-6 text-lg-left text-center">
-            <div className="copyright-text">
-              <p className="text-muted m-0">
-                Copyright © {new Date().getFullYear()}, All Right Reserved{" "}
-                <a
-                  className="a-white"
-                  href="https://www.smashcode.dev"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Smash Code
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="m-0 flex items-center justify-center bg-primary-gray p-4 text-sm">
+      <p className=" text-light-gray ">
+        Copyright © {new Date().getFullYear()}, All Right Reserved{" "}
+        <Link
+          className="text-primary-green"
+          href="https://www.smashcode.dev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Smash Code
+        </Link>
+      </p>
     </div>
   );
 };
