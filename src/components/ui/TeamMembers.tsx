df5c9adbc80266b3e93d12bcd7d12116832b5a09
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2023-12-23 19:01:49 +0500

    Improve performance

--- src/components/ui/TeamMembers.tsx
+++ src/components/ui/TeamMembers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { SectionHeading, UserCard } from "@/components";
+import { Loader, SectionHeading, UserCard } from "@/components";
 import { teamMembers } from "@/data/members";
 
 const TeamMembers: React.FC = () => {
@@ -9,16 +9,22 @@ const TeamMembers: React.FC = () => {
     <div className="px-4">
       <SectionHeading subHeading="Meet out" heading="Team" />
       <div className="style-right-border team-member-main flex flex-wrap items-center px-5">
-        {teamMembers.map(({ memberName, role, imageUrl }, key) => (
-          <div className="col-md-4 w-full md:w-1/2 lg:w-1/3" key={key}>
-            <UserCard
-              type=""
-              memberName={memberName}
-              role={role}
-              imageUrl={imageUrl}
-            />
-          </div>
-        ))}
+        {teamMembers?.length > 0 ? (
+          teamMembers.map(({ memberName, role, imageUrl }, key) => {
+            return (
+              <div className="col-md-4 w-full md:w-1/2 lg:w-1/3" key={key}>
+                <UserCard
+                  type={""}
+                  memberName={memberName}
+                  role={role}
+                  imageUrl={imageUrl}
+                />
+              </div>
+            );
+          })
+        ) : (
+          <Loader type="spinner" />
+        )}
       </div>
     </div>
   );
