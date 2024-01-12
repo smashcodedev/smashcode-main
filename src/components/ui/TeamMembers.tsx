35d321fc171b43de83d42bf3a2574fbd86d0ac26
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2024-01-12 22:25:52 +0500

    Add filter in team members page

--- src/components/ui/TeamMembers.tsx
+++ src/components/ui/TeamMembers.tsx
@@ -2,35 +2,37 @@ import React from "react";
 import { SectionHeading, UserCard } from "@/components";
 import { getTeamMembers } from "@/api/apiTeamMembers";
 
+type TeamMemberType = {
+  memberName: string;
+  role: string;
+  Image_URL: string;
+  featured?: boolean;
+};
+
 const TeamMembers: React.FC = async () => {
   const teamMembers = await getTeamMembers();
-  console.log(teamMembers);
-  const filteredMembers = Object.values(teamMembers).filter(
-    (member: any) => member.featured === true,
-  );
+  const filteredMembers = Object.values(
+    teamMembers as Record<string, TeamMemberType>,
+  ).filter((member) => member.featured === true);
 
   return (
     <div className="mt-12 px-4">
       <SectionHeading subHeading="Meet out" heading="Team" />
       <div className="style-right-border team-member-main flex flex-wrap items-center px-5">
-        {(
-          filteredMembers as Array<{
-            memberName: string;
-            role: string;
-            Image_URL: string;
-          }>
-        ).map(({ memberName, role, Image_URL }, key) => {
-          return (
-            <div className="col w-full sm:w-1/2 md:w-1/3" key={key}>
-              <UserCard
-                type={""}
-                memberName={memberName}
-                role={role}
-                Image_URL={Image_URL}
-              />
-            </div>
-          );
-        })}
+        {(filteredMembers as Array<TeamMemberType>).map(
+          ({ memberName, role, Image_URL }, key) => {
+            return (
+              <div className="col w-full sm:w-1/2 md:w-1/3" key={key}>
+                <UsrCard
+                  type={""}
+                  memberName={memberName}
+                  role={role}
+                  Image_URL={Image_URL}
+                />
+              </div>
+            );
+          },
+        )}
       </div>
     </div>
   );
