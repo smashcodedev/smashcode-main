aebf42e97ff1cd91f4221596086d1ea251f373f6
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2024-01-24 15:58:12 +0500

    Improve Footer styling

--- src/components/Footer/ContactInfo.tsx
+++ src/components/Footer/ContactInfo.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BsEnvelope, BsPhone } from "react-icons/bs";
 import { FaMapMarkerAlt } from "react-icons/fa";
 
@@ -14,18 +13,24 @@ const contactInfo: ContactInfoTypes[] = [
   { label: "Email", info: "smashcode.dev@gmail.com", icon: <BsEnvelope /> },
 ];
 
-const ContactInfo: React.FC = () => {
+const ContactInfo = () => {
   return (
     <div className="relative w-full border-b border-b-[#373636] pb-4 pt-5 md:flex">
       <div className="-mx-2 md:flex justify-between w-full items-center">
         {contactInfo.map(({ label, info, icon }) => (
-          <div key={label} className="md:w-1/3 flex items-center justify-center md:gap-4 gap-2">
-            <div className="min-h-[50px] w-[10%] text-center text-4xl text-primary-green">
+          <div
+            key={label}
+            className="md:w-1/3 flex items-center justify-center">
+            <div className="min-h-[50px] w-[10%] text-center flex justify-center md:justify-start text-[1.4rem] sm:text-[2rem] text-primary-green">
               {icon}
             </div>
             <div className="w-5/6 pl-2">
-              <h4 className="text-xl font-semibold text-white">{label}</h4>
-              <span className="text-lg text-light-gray">{info}</span>
+              <h4 className="text-[1rem] font-medium sm:font-semibold text-white sm:text-[1.2rem]">
+                {label}
+              </h4>
+              <span className="text-[0.9rem] sm:text-[1.1rem] text-light-gray">
+                {info}
+              </span>
             </div>
           </div>
         ))}
