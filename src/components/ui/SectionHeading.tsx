d07cc6cf4279c85c3fbb8f79e32612cde950bd2c
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2024-02-25 22:38:46 +0500

    Make page more accessible

--- src/components/ui/SectionHeading.tsx
+++ src/components/ui/SectionHeading.tsx
@@ -8,7 +8,7 @@ const SectionHeading: React.FC<HeadingProps> = ({
 }) => {
   return (
     <div>
-      <h3 className="text-2xl pb-4">{subHeading}</h3>
+      <h2 className="text-2xl pb-4">{subHeading}</h2>
       <h2 className={`section-heading pb-5 text-6xl`}>{heading}</h2>
       <p className="p-4 px-1 w-full text-lg md:w-8/12">
         {description}
