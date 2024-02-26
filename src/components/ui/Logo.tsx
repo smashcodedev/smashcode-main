2515687ca6fab0e6c25e25763562216e80584fb3
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2024-02-26 21:39:12 +0500

    Fix colors issue

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/../public/logo.png";
+import { usePathname } from "next/navigation";
 
 const Logo = () => {
+  const pathname = usePathname();
+
   return (
-    <Link href="/">
+    <Link href="/" prefetch={pathname.startsWith("/projects") ? false : true}>
       {/* <Link href="/home"> */}
       <Image
         src={logo}
