15c7e15bca9516947bf6e99598acb53900f573de
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2024-02-27 18:10:31 +0500

    Fix defer images issue

--- src/components/Navbar/NavMobile.tsx
+++ src/components/Navbar/NavMobile.tsx
@@ -81,10 +81,10 @@ const NavMob: React.FC<NavMobProps> = ({ links }) => {
               <Image
                 src="https://firebasestorage.googleapis.com/v0/b/smash-code.appspot.com/o/others%2Flogo.png?alt=media&token=03ad23b2-117d-405e-b75d-cf6ff119274f"
                 alt="smash code logo"
-                quality={100}
                 width={65}
                 height={65}
                 className="w-24 cursor-pointer object-contain"
+                loading="lazy"
               />
             </Link>
           </div>
