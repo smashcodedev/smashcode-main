52d3449931c63cd9f82755f54b7d20eed5eeff34
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2024-03-13 19:39:54 +0500

    Fix deployment issue

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,4 +1,4 @@
-import { ServiceHeader, ServicesList } from "@/components";
+import { ServicesList } from "@/components";
 import servicesData, { ServiceType } from "@/data/services";
 
 const services: ServiceType[] = [
@@ -15,11 +15,19 @@ const ServicesPage: React.FC = () => {
   return (
     <section className="m-10 mx-auto w-full lg:max-w-7xl">
       <div className="m-7 mt-32 xl:mt-44 2xl:ml-0">
-        <ServiceHeader
-          subHeading="Services we"
-          heading="Offered"
-          description="Responsive Websites & Web Apps, and Full Stack Web Apps Development."
-        />
+        <h2 className="pb-4 text-2xl">Services we</h2>
+        <h2 className="section-heading pb-5 text-6xl">Offered</h2>
+        <p className="w-full p-4 px-1 text-lg md:w-8/12">
+          Responsive Websites & Web Apps, and Full Stack Web Apps Development.
+        </p>
+
+        <ul className="list-disc px-3">
+          <li>Custom Web Development ( HTML, CSS, JS ReactJs, Firebase)</li>
+          <li>
+            Blockchain Web Apps (Minting, Wallet Connectivity, Smart Contract
+            Integration)
+          </li>
+        </ul>
 
         <ServicesList services={services} />
       </div>
