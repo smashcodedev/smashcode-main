7740bb7f493f09fa3d0556434ab0c024626b6998
smashcodedev <devsmashcode@gmail.com>
2024-03-21 01:40:00 +0500

    new updates: video added instead of sparkles

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -1,17 +1,16 @@
 import Link from "next/link";
-import SparklesInit from "./SparklesInit";
 
 const HeroSection: React.FC = () => {
   return (
     <section className="m-8 mt-32 2xl:mt-32">
-      <div className="flex flex-col items-center justify-center text-center md:flex-row md:text-left">
+      <div className="flex flex-col items-center justify-center text-center md:flex-row md:justify-between md:text-left">
         <div className="space-y-6">
           <h1 className="text-center text-2xl capitalize md:text-left md:text-6xl md:leading-tight">
             What you can think <br /> we can develop it <br />
             <span className="section-heading pb-5">for you!</span>
           </h1>
           <div className="flex flex-col space-y-6">
-            <p className="mx-4 text-base sm:text-xl md:mx-0">
+            <p className="mx-4 max-w-[600px] text-base sm:text-xl md:mx-0">
               We create future-ready websites & web apps via innovative digital
               solutions so that you can focus on your core business.
             </p>
@@ -26,12 +25,17 @@ const HeroSection: React.FC = () => {
           </div>
         </div>
 
-        <SparklesInit />
-        <div className="relative text-center">
-          <canvas
-            className="mx-auto my-auto h-[300px] w-[300px] text-center md:h-[400px] md:w-[400px] lg:h-[500px] lg:w-[500px]"
-            id="canvas"
-          ></canvas>
+        <div className="relative px-3 text-center md:w-[40%]">
+          <div className="absolute left-0 top-0 h-full w-full"></div>
+          <video
+            id="sparkles-animation"
+            className="pointer-events-none z-[-1] mx-auto w-auto max-w-full"
+            src="/sparkles.webm"
+            playsInline
+            loop
+            muted
+            autoPlay
+          />
         </div>
       </div>
     </section>
